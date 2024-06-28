c94857423217e9e5f73ceaf8dea15c1a0a82c0b0
kimcorneil <kcorneil3713@gmail.com>
2024-06-27 21:31:54 -0400

    My master plan added.

--- Assignment3.R
+++ Assignment3.R
@@ -2,3 +2,17 @@
 # BTC 1855H
 # Kimberly Corneil
 # July 2, 2024
+
+## My master plan for a super fun game of hangman
+#' Make a txt file on my mac and save it as "words.txt"
+#' Read the word file with R
+#' Use sample() to choose the word (it is secret so that you can play too)
+#' Using nchar() let the user know the word is 7 letters long
+#' I have decided to be nice so I will let the user know they have 10 tries 
+#' Finally, I will ask them for their input with the readline() prompt 
+#' I will check that the input is a single letter character
+#' If the letter is in the word I will let them know they have found a letter and where in the word it is
+#' If the letter is not in the word I will tell them sadly that they have not found a letter and ask them to try again
+#' I will let them know each time how many attempts they have left
+#' If they fail I will let them know they have lost and reveal the secret word
+#' If they get it I will let them know they are a winner :))
