22f2172adb490e2e361c7c40962bc5d16febd247
kimcorneil <kcorneil3713@gmail.com>
2024-06-28 12:00:01 -0400

    Script in R to make the possible words for hangman text file

---
+++ WordsScript.R
@@ -0,0 +1,9 @@
+# Script for words for assignment 3
+my_words <- c("chicken", "sizzle", "cabbage", "iceberg", "macaron", "pacific", "rabbits", "ukelele", "vaccine")
+
+
+# Make my words a one column data frame
+hangman2 <- data.frame(my_words)
+
+# view hangman2
+View(hangman2)
