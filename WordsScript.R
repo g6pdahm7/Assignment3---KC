de297937d3a7c75ef7a3115488b3a0cea07c5df5
kimcorneil <kcorneil3713@gmail.com>
2024-06-28 12:04:32 -0400

    Fixed what was first hangman2 so that hangman3 is a text file in the format I desire.

--- WordsScript.R
+++ WordsScript.R
@@ -7,3 +7,10 @@ hangman2 <- data.frame(my_words)
 
 # view hangman2
 View(hangman2)
+
+# export hangman2 as a 1 column text file
+# use sep = \t to separate the words by tabs
+# call it hangman3 because I made hangman2 earlier but did not like the format
+# use row.names and col.names = F to remove names 
+# use quote = F so that quotes are not around the words
+write.table(hangman2, file = "hangman3.txt", row.names = F, col.names = F, quote= F, sep = "\t")
